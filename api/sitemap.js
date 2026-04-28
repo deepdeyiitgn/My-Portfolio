8801a5007bf7f1ecb218931357456011ed612093
Deep Dey <deydeep200821@gmail.com>
2026-04-28 21:25:47 +0530

    Update sitemap.js
    28/04/2026 || 9.26pm

--- api/sitemap.js
+++ api/sitemap.js
@@ -30,6 +30,7 @@ export default async function handler(req, res) {
         'transparent-clock', 
         'quicklink', 
         'studybot', 
+        'personal-portfolio',
         'qlynk-node-server'
       ];
       
